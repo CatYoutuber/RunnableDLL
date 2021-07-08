0ead9ee45250732d6b68959cbae13b56f90a4002
CatYoutuber <66025258+CatYoutuber@users.noreply.github.com>
2021-07-08 20:08:27 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <Windows.h>
+
+extern "C" __declspec(dllexport) int _stdcall Entry
+(HWND hWnd, HINSTANCE hInst, LPSTR lpszCmdLine, int nCmdShow)
+{
+	// TODO: Your Code Here [зез]
+	return 0;
+}
